2df766eec853805f831b3ac425e3dafe4ab7dd2d
VasireddyGanesh <vasireddi.ganesh.8@gmail.com>
2021-12-19 17:38:59 +0530

    fixed bugs

--- About.php
+++ About.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <title>Find Blood Donor | About</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/about.css">
+    <link rel="stylesheet" type="text/css" href="css/About.css">
 </head>
 
 <body>
--- contact.php
+++ contact.php
@@ -1,7 +1,5 @@
 
 <?php
-    session_start();
-
     $server = "localhost";
     $user = "root";
     $password = "";
